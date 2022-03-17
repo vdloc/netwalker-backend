f751a019f99ce3dc0917dccd5c9a06d232d0adc0
vdloc <uvcudlco@gmail.com>
2022-03-17 22:26:09 +0700

    add cronjob to package.json

--- jobs/index.js
+++ jobs/index.js
@@ -1,9 +1,10 @@
-const cron = require("node-cron");
-const redditJob = require("./reddit");
+require('dotenv').config();
+const cron = require('node-cron');
+const redditJob = require('./reddit');
 
-cron.schedule("0 0 * * *", async () => {
-	await redditJob();
-	console.log(`Cron job success`);
+cron.schedule('0 0 * * *', async () => {
+  await redditJob();
+  console.log(`Cron job success`);
 });
 
 module.exports = cron;
